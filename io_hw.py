11be7baf574ca0ee3cb279b01585eb9c66c8b9fd
vincentahwang <vincentahwang@gmail.com>
2019-03-08 01:06:04 +0000

    This is a test commit of the io_hw.py

--- io_hw.py
+++ io_hw.py
@@ -4,8 +4,15 @@ import seaborn as sns
 def io_hw(out_path):
     '''
     This is your homework assignment.
-    You will be wrting a function here.
+    You will be writing a function here.
+    '''
+    file_place = "C:\Users\Vincent\Dropbox\College\Grad\Spring\Data Science\DataSets\project-sunroof-census_tract.csv"
+    df = pd.read_csv(file_place)
+    print(df)
     
+    head_df = data.head(df)
+    
+    ''''
     Your input will be:
     
     out_path: a string that is the path to output a csv of the head of your data.
