4786aac7fa3148e675b55d3e71996a7862ed952a
Ottillia Ni <ottilliani@Ottillias-MacBook-Pro.local>
2019-03-13 19:38:40 -0400

    added url

--- io_hw.py
+++ io_hw.py
@@ -16,7 +16,7 @@ def io_hw(out_path):
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
-    df = pd.read_csv('./hfi_cc_2018.csv')
+    df = pd.read_csv('https://tufts.box.com/shared/static/7iwsgxhffhfs87v209scqihq57pnmev0.csv')
     
     head_df = df.head(5)
     head_df.to_csv(out_path)
