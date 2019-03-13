bca124f89e9866995f5c93284ddc481a9ed48075
vincentahwang <48077850+vincentahwang@users.noreply.github.com>
2019-03-12 21:25:39 -0400

    Update io_hw.py

--- io_hw.py
+++ io_hw.py
@@ -2,18 +2,8 @@ import pandas as pd
 import seaborn as sns
 
 def io_hw(out_path):
-    '''
-    This is your homework assignment.
-    You will be writing a function here.
-    '''
-    file_place = "C:\Users\Vincent\Dropbox\College\Grad\Spring\Data Science\DataSets\project-sunroof-census_tract.csv"
-    df = pd.read_csv(file_place)
-    print(df)
-    
-    head_df = data.head(df)
-    
-    ''''
-    Your input will be:
+    ''' 
+     Your input will be:
     
     out_path: a string that is the path to output a csv of the head of your data.
     
@@ -22,4 +12,10 @@ def io_hw(out_path):
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
+    filepath = 'https://www.dropbox.com/s/egrqg5hcja6i5r5/ACSDATAFinal.csv?dl=0'
+    df = pd.read_csv(filepath)
+    head_df = df.head()
+    output = head_df.to_csv(outpath)
+    print(type(head_df))
+    
     return df, head_df
