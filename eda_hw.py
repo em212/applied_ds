63ffa65275d7d18521ea7ccc30885e6c4420a2e0
Hamid Reza Mohebbi <mohebbi@mings-mac-pro.sci-1.umb.edu>
2019-03-29 16:02:34 -0400

    adding io and eds files.

--- eda_hw.py
+++ eda_hw.py
@@ -14,6 +14,7 @@ def average(series):
     See numpy documenation for implementation details:
     https://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
     """
+    
     return float(sum(series))/len(series)
 
 def standard_deviation(series):
