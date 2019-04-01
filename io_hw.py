b0cefcaf4fc109171cf5159a02477a633a223c9c
tleong01 <tyler.leong@tufts.edu>
2019-03-31 22:28:12 -0400

    correct replica files

--- io_hw.py
+++ io_hw.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import seaborn as sns
-import test_io_hw
-import os
-
 
 def io_hw(out_path):
     '''
@@ -18,7 +15,4 @@ def io_hw(out_path):
     df: The full pandas dataframe of your dataset.
     head_df: A new dataframe that is a copy of the first 5 lines of your dataframe, df.
     '''
-    df = pd.read_csv('profilesShort.csv')
-    head_df = df.head(5)
-    head_df.to_csv(out_path)
     return df, head_df
